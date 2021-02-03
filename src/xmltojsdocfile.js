7fd100f85acd2231c5c9aac30add653451b87a96
Lukáš Drahník <ldrahnik@gmail.com>
2021-02-03 15:54:37 +0100

    fix import @ldrahnik/xmltojsdoc
    Signed-off-by: Lukáš Drahník <ldrahnik@gmail.com>

--- src/xmltojsdocfile.js
+++ src/xmltojsdocfile.js
@@ -2,7 +2,7 @@
 
 var fs = require("fs");
 var path = require("path");
-var XMLToJsDoc = require("@peopleinneed/xmltojsdoc");
+var XMLToJsDoc = require("@ldrahnik/xmltojsdoc");
 
 var defaultOptions = exports.defaultOptions = {
     commentIsBelowDefinition: false
